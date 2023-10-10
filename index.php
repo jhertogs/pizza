8e6d719e89c265f0644707732fd1989665a20d99
Josse Hertogs <jossehertogs@gmail.com>
2023-10-10 10:30:40 +0200

    put everything in 1 big form

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-
+        <title>Pizza di php</title>
     </head>
     <?php 
 $totalprice = 0.00;
@@ -13,6 +13,37 @@ $day = date("l");
 
 
 if(isset($_POST["submit"])){
+        $amount_marg = floatval($_POST["marg"]);
+        $amount_fung = floatval($_POST["fung"]);
+        $amount_mari = floatval($_POST["mari"]);
+        $amount_hawi = floatval($_POST["hawi"]);
+        $amount_quat = floatval($_POST["quat"]);
+        if($day == "Monday"){
+            $price_margs = $amount_marg * 7.50;
+            $price_fungis = $amount_fung * 7.50;
+            $price_maris = $amount_mari * 7.50;
+            $price_hawis = $amount_hawi * 7.50;
+            $price_quats = $amount_quat * 7.50; 
+            $totalprice = $price_fungis + $price_hawis + $price_margs + $price_maris + $price_quats;
+        }  
+        else{
+            $price_margs = $amount_marg * $piz_marg;
+            $price_fungis = $amount_fung * $piz_fung;
+            $price_maris = $amount_mari * $piz_mari;
+            $price_hawis = $amount_hawi * $piz_hawi;
+            $price_quats = $amount_quat * $piz_quat;
+            $totalprice = $price_fungis + $price_hawis + $price_margs + $price_maris + $price_quats;
+        }
+        $totalprice = $price_fungis + $price_hawis + $price_margs + $price_maris + $price_quats;
+        if(($day == "Friday")&& ($totalprice > 20)){
+            $price_margs = ($amount_marg*$piz_marg)-(($amount_marg * $piz_marg)/100)*15;
+            $price_fungis = ($amount_fung*$piz_fung)-(($amount_fung * $piz_fung)/100)*15;
+            $price_maris = ($amount_mari*$piz_mari)-(($amount_mari * $piz_mari)/100)*15;
+            $price_hawis = ($amount_hawi*$piz_hawi)-(($amount_hawi * $piz_hawi)/100)*15;
+            $price_quats = ($amount_quat*$piz_quat)-(($amount_quat * $piz_quat)/100)*15;
+            $totalprice = $price_fungis + $price_hawis + $price_margs + $price_maris + $price_quats;
+            
+        }
     if(empty($_POST["naam"])){
         $naam_empty = "U moet een naam invullen!";
     } else{
@@ -50,40 +81,7 @@ if(isset($_POST["submit"])){
     }
     }
 
-    if(isset($_POST["submit2"])){
-        
-        $amount_marg = floatval($_POST["marg"]);
-        $amount_fung = floatval($_POST["fung"]);
-        $amount_mari = floatval($_POST["mari"]);
-        $amount_hawi = floatval($_POST["hawi"]);
-        $amount_quat = floatval($_POST["quat"]);
-        if($day == "Monday"){
-            $price_margs = $amount_marg * 7.50;
-            $price_fungis = $amount_fung * 7.50;
-            $price_maris = $amount_mari * 7.50;
-            $price_hawis = $amount_hawi * 7.50;
-            $price_quats = $amount_quat * 7.50; 
-            $totalprice = $price_fungis + $price_hawis + $price_margs + $price_maris + $price_quats;
-        }  
-        else{
-            $price_margs = $amount_marg * $piz_marg;
-            $price_fungis = $amount_fung * $piz_fung;
-            $price_maris = $amount_mari * $piz_mari;
-            $price_hawis = $amount_hawi * $piz_hawi;
-            $price_quats = $amount_quat * $piz_quat;
-            $totalprice = $price_fungis + $price_hawis + $price_margs + $price_maris + $price_quats;
-        }
-        $totalprice = $price_fungis + $price_hawis + $price_margs + $price_maris + $price_quats;
-        if(($day == "Friday")&& ($totalprice > 20)){
-            $price_margs = ($amount_marg*$piz_marg)-(($amount_marg * $piz_marg)/100)*15;
-            $price_fungis = ($amount_fung*$piz_fung)-(($amount_fung * $piz_fung)/100)*15;
-            $price_maris = ($amount_mari*$piz_mari)-(($amount_mari * $piz_mari)/100)*15;
-            $price_hawis = ($amount_hawi*$piz_hawi)-(($amount_hawi * $piz_hawi)/100)*15;
-            $price_quats = ($amount_quat*$piz_quat)-(($amount_quat * $piz_quat)/100)*15;
-            $totalprice = $price_fungis + $price_hawis + $price_margs + $price_maris + $price_quats;
-            
-        }
-    }
+    
 
  function checkinp ($input) {
         $input = trim($input);
@@ -101,17 +99,15 @@ if(isset($_POST["submit"])){
             Plaats: <input type="text" name="plaats"><br><br>
             Bezorgen: <input type="radio" name="bezorgen-afhalen" value="bezorgen"> afhalen: <input type="radio" name="bezorgen-afhalen" value="afhalen"><br><br>
             Datum: <input type="datetime-local" name="datum"><br><br>
-            <input type="submit" name="submit">
-
-        </form>
         <h3>the pizza's</h3>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post"> 
+         
             Pizza margherita: <input type="number" min="0" max="10" name="marg"><br><br>
             Pizza funghi: <input type="number" min="0" max="10" name="fung"><br><br>
             Pizza marina: <input type="number" min="0" max="10" name="mari"><br><br>
             Pizza hawaii: <input type="number" min="0" max="10" name="hawi"><br><br>
             Pizza quattro formaggi: <input type="number" min="0" max="10" name="quat"><br><br>
-            <input type="submit" name="submit2">
+            <input type="submit" name="submit">
+
         </form>
 
         <?php 
