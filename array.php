844a2ad813573e6562af7bcda6b3f1c85cf7011f
Josse Hertogs <jossehertogs@gmail.com>
2024-03-21 12:10:10 +0100

    array stores data from database

--- array.php
+++ array.php
@@ -9,24 +9,25 @@ try {
     $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}  catch(PDOException $e) {
-    echo $sql . "<br>" . $e->getMessage();
-}
+    
+    $sql = "SELECT code, name, price FROM pizzas";
+    $stmt = $pdo->query($sql);
 
-$sql = "SELECT code, name, price FROM pizzas";
-$result = $conn->query($sql);
-
-if($result->num_rows > 0){
-    while ($row = $result->fetch_assoc()) {
-        $pizzaDetails[$row['code']] = ['name' => $row['name'], 'price' => $row['price']];
+    $pizzaDetails = [];
+    if ($stmt->rowCount() > 0) {
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $pizzaDetails[$row['code']] = ['name' => $row['name'], 'price' => $row['price']];
+        }
     }
+} catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
 }
-$conn->close();
+
+// Close the connection
+unset($pdo);
 
 /*
-
 ||old array, new array (above) now dynamically stores whatever pizzas are stored in the database||
-
 $pizzaDetails = [
     'marg' => ['name' => 'Pizza Margherita', 'price' => 12.50],
     'fung' => ['name' => 'Pizza Funghi', 'price' => 12.50],
