d5de0c07a3c263a4809b0d60914a7cb4e131c356
Josse Hertogs <jossehertogs@gmail.com>
2024-03-21 12:08:43 +0100

    PizzaDetials array now gets made with the info stored in the databse (yes gpt helped with this)

--- array.php
+++ array.php
@@ -1,6 +1,31 @@
 <?php 
 
- 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "PizzaDB";
+
+try {
+    $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}  catch(PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
+}
+
+$sql = "SELECT code, name, price FROM pizzas";
+$result = $conn->query($sql);
+
+if($result->num_rows > 0){
+    while ($row = $result->fetch_assoc()) {
+        $pizzaDetails[$row['code']] = ['name' => $row['name'], 'price' => $row['price']];
+    }
+}
+$conn->close();
+
+/*
+
+||old array, new array (above) now dynamically stores whatever pizzas are stored in the database||
 
 $pizzaDetails = [
     'marg' => ['name' => 'Pizza Margherita', 'price' => 12.50],
@@ -12,6 +37,6 @@ $pizzaDetails = [
 ];
 
 
-
+*/
 
 ?>
